ab0cf48376ecc088b13475a5983f915801a522ab
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2018-06-17 19:06:07 -0400

    added basic ImageDescription component

---
+++ src/common/ImageDescription.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+let IDContainer;
+export default class ImageDescription extends React.Component{
+    constructor(props) {
+        super(props);
+        IDContainer = styled.div`
+            position: relative;
+            width: ${props.width};
+            height: auto;
+        `;
+    }
+    render(){
+        return (
+            <IDContainer>
+                <img
+                    className="IDImage"
+                    src={this.props.img}
+                    alt="" />
+                <div className="blur" />
+                <div id="title">
+                    {this.props.title}
+                </div>
+            </IDContainer>
+        )
+    }
+}
